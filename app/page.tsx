1f0bab2bba9f64a9cad77fd1a09bd57372d6341d
hasan-furkan <hsnfrkn32@gmail.com>
2024-11-18 18:32:27 +0100

    fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,9 @@ import { redirect } from "next/navigation";
 export default function Home() {
   redirect("/dashboard/crm");
 
-  return null;
+  return (
+    <div>
+      <AppSidebar />
+    </div>
+  );
 }
